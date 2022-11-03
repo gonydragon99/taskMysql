dad64c79c0730e14790ff18885be8a3314d815e7
gonzalo <gonpm08@hotmail.com>
2022-11-03 00:06:36 -0400

    corregido

---
+++ src/main/java/singleton/MySqlDBClient.java
@@ -0,0 +1,75 @@
+package singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
+
+public class MySqlDBClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(MySqlDBClient.class);
+
+    public static MySqlDBClient client = null;
+    private Connection connection;
+
+    /**
+     * Builds an instance of {@link MySqlDBClient}.
+     */
+    private MySqlDBClient() {
+        this.initializeMysqlDbClient();
+    }
+
+    /**
+     * Inicializando Mysql DB client.
+     */
+    private void initializeMysqlDbClient() {
+        try {
+            logger.info("Initializing the mysql client!!");
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bootcampDb", "root", "secret");
+            logger.info("Mysql Client was connected successfully");
+        } catch (SQLException e) {
+            logger.error("Error while connecting to MYSQL");
+        }
+    }
+
+    /**
+     * Gets the client.
+     *
+     * @return The client
+     */
+    public static MySqlDBClient getClient() {
+        if (Objects.isNull(client)) {
+            client = new MySqlDBClient();
+        }
+
+        return client;
+    }
+
+    /**
+     * Gets the data (Returns the current timestamp).
+     *
+     * @return The data
+     */
+    public long getData() {
+        long timestamp = 0;
+        try {
+            Statement stmt = this.connection.createStatement();
+            String query = "SELECT UNIX_TIMESTAMP(NOW(3))*1000 AS TIMESTAMP";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                timestamp = rs.getLong("TIMESTAMP");
+                logger.info("TIMESTAMP: {}", timestamp);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return timestamp;
+    }
+}
--- src/main/java/singleton/SingletonMain.java
+++ src/main/java/singleton/SingletonMain.java
@@ -1,15 +1,24 @@
 package singleton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SingletonMain {
 
-    public static void main(String[]arg){
+    private static final Logger logger = LoggerFactory.getLogger(SingletonMain.class);
+
+
+    public static void main(String[] arg) {
+        // Mongo DB Client.
         MongoDBClient mongoDBClient = MongoDBClient.getClient();
-        System.out.println(mongoDBClient.getData());
+        logger.info("mongo data: {}", mongoDBClient.getData());
         MongoDBClient mongoDBClient2 = MongoDBClient.getClient();
-        System.out.println(mongoDBClient2.getData());
-        MongoDBClient mongoDBClient3 = MongoDBClient.getClient();
-        System.out.println(mongoDBClient3.getData());
+        logger.info("mongo data2: {}", mongoDBClient2.getData());
 
-
+        // MYSQL DB_Client.
+        MySqlDBClient mySqlDBClient = MySqlDBClient.getClient();
+        logger.info("mysql data: {}", mySqlDBClient.getData());
+        MySqlDBClient mySqlDBClient2 = MySqlDBClient.getClient();
+        logger.info("mysql data2: {}", mySqlDBClient2.getData());
     }
 }
